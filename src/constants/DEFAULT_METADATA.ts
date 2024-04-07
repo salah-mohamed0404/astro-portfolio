9cfaac740ff4886d81506d53dee6eb6b7a8d132e
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-04-07 22:22:49 +0200

    Add default metadata constants for English and Arabic

---
+++ src/constants/DEFAULT_METADATA.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_TITLE = {
+	EN: "Salah | Frontend Developer",
+	AR: "صلاح | مطور واجهة المستخدم",
+};
+
+export const DEFAULT_DESCRIPTION = {
+	EN: "Welcome! I'm a Front-End Developer passionate about JavaScript and ReactJS. Explore my coding journey and projects here. Let's embark on a digital adventure together!",
+	AR: "مرحبًا في معرض اعمالي! اكتشف مهاراتي ومشاريعي، حيث أنا متخصص في لغات البرمجة مثل JavaScript و ReactJS. كمطور واجهة مستخدم، أحب أن أصمم تجارب واجهة ويب تكون مشوقة وسهلة الاستخدام. تفضل بزيارة أعمالي لترى كيف أقدم الابتكار في نظم المعلومات من خلال تصاميم مبتكرة، وواجهات متجاوبة، وتفاعلات مستخدم سلسة. دعنا نتواصل ونتحاور حول كيف يمكنني المساهمة في مشروع تطوير الويب القادم الخاص بك.",
+};
